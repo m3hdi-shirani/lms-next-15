0d61aec5d0712de27dd1ce00a61b0abd483196a9
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-14 16:26:41 +0330

    feat(layout): integrate Iranyekan font with RTL support in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
 
 import "./globals.css";
 
@@ -15,13 +16,48 @@ const figtree = Figtree({
   weight: ["300", "400", "500", "600", "700", "700", "800", "900"],
 });
 
+const iranyekan = localFont({
+  src: [
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-DemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-iranyekan",
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html className={`${figtree.variable} dark`}>
+    <html
+      dir="rtl"
+      className={`dark ${figtree.variable} ${iranyekan.variable}`}
+    >
       <body className="min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
         <main>{children}</main>
       </body>
