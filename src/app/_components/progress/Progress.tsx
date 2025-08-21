3283ba23ba72465c4aeaf968ecc5572d79312583
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-21 20:04:41 +0330

    feat(progress): implement Progress component with styles and types

---
+++ src/app/_components/progress/Progress.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames";
+import { Size } from "../types/size.type";
+import { ProgressProps } from "./progress.types";
+
+const sizeClasses: Record<Size, string> = {
+  tiny: "progress-xs",
+  small: "progress-sm",
+  normal: "progress-md",
+  large: "progress-lg",
+};
+
+const Progress = ({
+  variant = "neutral",
+  className,
+  size = "small",
+  value,
+}: ProgressProps) => {
+  const classes = classNames("progress", className, {
+    [`progress-${variant}`]: variant,
+    [`${sizeClasses[size]}`]: size,
+  });
+
+  return <progress value={value} max="100" className={classes} />;
+};
+
+export default Progress;
