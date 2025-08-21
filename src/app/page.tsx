d48b987e7ed393870bc9b60fcbc54f589f78b91b
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-21 15:29:06 +0330

    feat(home): add testimonials section with TestimonialList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { API_URL } from "@/config/global";
 import { homeFeatures } from "@/data/home-features";
+import { testimonials } from "@/data/testimonials";
 import { BlogPostSummary } from "@/types/blog-post-summery.interface";
 import { Suspense } from "react";
 import BlogPostCardList from "./(blog)/_components/BlogPostCardList";
@@ -9,6 +10,7 @@ import { Feature } from "./_components/feature";
 import HomeHeroSection from "./_components/home-hero-section/HomeHeroSection";
 import { IconArrowLeftFill } from "./_components/icons/icons";
 import { CardPlaceholder } from "./_components/placeholders/card";
+import { TestimonialList } from "./_components/testimonial";
 
 async function getNewestPosts(count: number): Promise<BlogPostSummary[]> {
   const res = await fetch(`${API_URL}/blog/newest/${count}`, {
@@ -106,6 +108,19 @@ export default async function Home() {
         </div>
         <BlogPostCardList posts={newestBlogPosts} />
       </section>
+
+      <div className="relative mt-32">
+        <div className="bg-primary pointer-events-none absolute bottom-0 left-1/2 aspect-square w-1/2 -translate-x-1/2 rounded-full opacity-5 -top-52 blur-3xl"></div>
+        <h2 className="text-info relative z-0 mx-auto text-3xl font-extrabold block w-fit">
+          <span className="-z-10 w-8 h-8 absolute bg-info opacity-25 -top-2 rounded-full inline-block -right-3"></span>
+          تجربه هم‌میسرهای کلاسبن
+        </h2>
+        <p className=" mb-32 text-lg text-center mt-2">
+          تو اینجا تنها نیستی. ببین هم‌مسیرهات نظرشون در مورد دوره‌های کلاسبن
+          چیه
+        </p>
+        <TestimonialList testimonials={testimonials} />
+      </div>
     </>
   );
 }
