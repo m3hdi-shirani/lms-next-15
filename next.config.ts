4b39ec5988a515fcb6252aa880204c4d9cfdd38d
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-21 09:20:33 +0330

    feat(next.config): configure image remote patterns for external sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.classbon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "minio-classbon.darkube.app",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
