d0c899ed831f0934fa900815157ccd1c109eeb55
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-22 12:50:05 +0330

    feat(types): add ProblemDetails and specific error interfaces

---
+++ src/types/http-errors.interface.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+
+interface ProblemDetails {
+  title: string;
+  statusCode: number;
+  detail?: string;
+  errors?: Record<string, string[]>;
+}
+
+interface BadRequestError extends ProblemDetails {}
+
+interface UnauthorizedError extends ProblemDetails {}
+
+interface ValidationError extends ProblemDetails {}
+
+interface NotFoundError extends ProblemDetails {}
+
+interface UnhandledError extends ProblemDetails {}
+
+interface networkError extends ProblemDetails {}
+
+export type {
+  BadRequestError,
+  networkError,
+  NotFoundError,
+  ProblemDetails,
+  UnauthorizedError,
+  UnhandledError,
+  ValidationError,
+};
