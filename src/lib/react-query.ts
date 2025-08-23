cd4d0d94717d10753d7a9c409c4edcea5a869b51
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-23 09:47:57 +0330

    feat(react-query): implement React-Query

---
+++ src/lib/react-query.ts
@@ -0,0 +1,24 @@
+import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      //show notification
+    },
+  }),
+
+  mutationCache: new MutationCache({
+    onError: (error: unknown) => {
+      //show notification
+    },
+  }),
+
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      throwOnError: false,
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
