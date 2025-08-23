b0d041c2d3a9bc3e5d7a6260935cc2253d73ff60
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-08-23 15:38:23 +0330

    feat(comments): implement infinite scrolling for course comments and add error handling

--- src/app/(courses)/courses/[slug]/_api/get-comments.ts
+++ src/app/(courses)/courses/[slug]/_api/get-comments.ts
@@ -1,5 +1,5 @@
 import { readData } from "@/core/http-service/http-service";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { CourseCommentList } from "../_types/course-comment.interface";
 
 type GetCommentsOptions = {
@@ -18,12 +18,38 @@ const getComments = ({
 };
 
 export const useCourseComments = ({ params }: GetCommentsOptions) => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["courseComments"],
-    queryFn: () => getComments({ params }),
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ["courseComments"],
+  //   queryFn: () => getComments({ params }),
+  //   staleTime: 5 * 60 * 60 * 1000, // 5 hours
+  //   gcTime: 6 * 60 * 60 * 1000, // 5 hours
+  // });
+
+  const {
+    data,
+    error,
+    isFetchingNextPage,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  } = useInfiniteQuery({
+    queryKey: ["courseComments", params.slug],
+    queryFn: ({ pageParam }) =>
+      getComments({ params: { ...params, page: pageParam } }),
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+    initialPageParam: 1,
     staleTime: 5 * 60 * 60 * 1000, // 5 hours
-    gcTime: 6 * 60 * 60 * 1000, // 5 hours
+    gcTime: 6 * 60 * 60 * 1000, // 6 hours
   });
 
-  return { data, isLoading };
+  return {
+    data,
+    error,
+    isFetchingNextPage,
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+  };
 };
